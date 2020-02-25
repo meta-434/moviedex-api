2221db27ceb3fa9067d0fa232e6558a20b9f38d9
meta-434 <ach1@vt.edu>
2020-02-25 13:59:21 -0500

    prep for deployment

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const MOVIES = require('./movies-data.json');
 
 const app = express();
 
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSetting))
 app.use(cors());
 app.use(helmet());
 
@@ -42,6 +43,20 @@ app.get('/movie', function handleGetMovie(req, res) {
     }
 
     res.json(response)
-})
+});
 
-module.exports = app;
+app.use((error, req, res, next) => {
+    let response;
+    if (process.env.NODE_ENV === 'production') {
+        response = { error: { message: 'server error' }}
+    } else {
+        response = { error }
+    }
+    res.status(500).json(response)
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`)
+});
--- server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const app = require('./app.js');
-
-const PORT = process.env.PORT;
-
-app.listen(PORT, () => {
-    console.log(`Server started on PORT ${PORT}`);
-});
